10452b8592a860b987b8ea08661335cc3746d60d
Maffin <maftuna.nova@gmail.com>
2021-11-21 17:59:28 +0500

    js-hw

---
+++ script.js
@@ -0,0 +1,54 @@
+// name
+
+var name = prompt('Enter your name') 
+
+alert(name)
+
+console.log(name);
+
+// age
+var age = prompt('Enter your age')
+
+alert(age)
+
+console.log(age);
+
+// tasks
+
+var plus = +prompt('9 + 7 = ?')
+
+alert(plus)
+
+console.log(' 9 + 7 = 16 ' + ' Your answer is ' +  plus);
+
+
+
+var minus = +prompt('9 - 7 = ?')
+
+alert(minus)
+
+console.log(' 9 - 7 = 2 ' + ' Your answer is ' +  minus);
+
+
+var multiply = +prompt('9 * 7 = ?')
+
+alert(multiply)
+
+console.log(' 9 * 7 = 63 ' + ' Your answer is ' +  multiply);
+
+
+var division = +prompt('63 / 9 = ?')
+
+alert(division)
+
+console.log(' 63 / 9 = 7 ' + ' Your answer is ' +  division);
+
+
+
+var percent = +prompt('9 % 7 = ?')
+
+alert(percent)
+
+console.log(' 9 % 7 = 2 ' + ' Your answer is ' +  percent);
+
+alert('Enter the console to see your answers')
